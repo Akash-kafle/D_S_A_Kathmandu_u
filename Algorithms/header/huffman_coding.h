16df07732dbd0f72497e864533de6a75e6ce9d04
Akash kafle <aakashkafle47@gmail.com>
2024-09-15 10:43:17 +0545

    added cmake

--- Algorithms/header/huffman_coding.h
+++ Algorithms/header/huffman_coding.h
@@ -1,27 +1,47 @@
 #pragma once
 #include <fstream>
+
+template <typename D>
+class Data
+{
+    D *data;
+};
+
 template <typename T>
 class Codding
 {
 private:
-    T *data;
+    Data<T> UserData;
     long long length_of_data{};
     std::string path_to_file;
 
 public:
-    bool Compress(T *data = nullptr);
+    bool Compress(Data<T> *data);
     T Decompress();
 };
 
 template <typename T>
-bool Codding<T>::Compress(T *data = nullptr)
+bool Codding<T>::Compress(Data<T> *data)
 {
     // parsing the data to be looked as bytes and saving
+    if (data == nullptr)
+    {
+        return false;
+    }
+
+    uint64_t bytes_data = Convert_to_bytes(data);
 }
 
 template <typename T>
 T Codding<T>::Decompress()
 {
-    
-    return
+    // De-parsing the data from compressed bytes to actual data
+
+    return T;
+}
+
+template <typename T>
+uint64_t Convert_to_bytes(Data<T> *data)
+{
+    return uint64_t{0};
 }
---
+++ Algorithms/src/huffman_coding.cpp
@@ -0,0 +1,7 @@
+#include "../header/huffman_coding.h"
+
+int main()
+{
+    printf("Hello world");
+    return 0;
+}
